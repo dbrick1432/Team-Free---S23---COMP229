599b19b8d8e0d7f0c4c9c6370c6069df436bf6e0
shinumolsebas <123741594+shinumolsebas@users.noreply.github.com>
2023-07-23 00:06:26 -0300

    List of Survey added

--- routes/index.js
+++ routes/index.js
@@ -17,8 +17,8 @@ router.get('/about', function(req, res, next) {
 });
 
 /* GET home page. */
-router.get('/products', function(req, res, next) {
-  res.render('index', { title: 'Products' });
+router.get('/list', function(req, res, next) {
+  res.render('list', { title: 'List of Surveys' });
 });
 
 /* GET home page. */
