fbecc602bb53a85fc3639d04789da2bc072f8c16
dbrick1432 <119401041+dbrick1432@users.noreply.github.com>
2023-07-29 19:21:35 -0700

    routing a login page

--- routes/index.js
+++ routes/index.js
@@ -30,5 +30,19 @@ router.get('/services', function(req, res, next) {
 router.get('/contact', function(req, res, next) {
   res.render('index', { title: 'Contact Us' });
 });
+/* Get Route for displaying the Login Page */
+router.get('/login', indexController.displayLoginPage);
+
+/* Post Route for processing the Login Page */
+router.post('/login', indexController.processLoginPage);
+
+/* Get Route for displaying the Register Page */
+router.get('/register', indexController.displayRegisterPage);
+
+/* Get Route for processing the Register Page */
+router.post('/register', indexController.processRegisterPage);
+
+/* Get to perform UserLogout */
+router.get('/logout', indexController.performLogout);
 
 module.exports = router;
